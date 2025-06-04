b716a225926909681a27f29b3836bc4c2dd1d0e1
LJM <1743390732@qq.com>
2025-06-04 11:52:31 +0800

    backend done

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,16 +2,19 @@ import { motion, AnimatePresence } from 'motion/react';
 import { useContext } from 'react';
 
 import FeedbackItem from './FeedbackItem';
+import Spinner from './shared/Spinner';
 import FeedbackContext from '../context/FeedbackContext';
 
 function FeedbackList() {
-  const { feedback } = useContext(FeedbackContext);
+  const { feedback, isLoading } = useContext(FeedbackContext);
 
-  if (!feedback || feedback.length === 0) {
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>No Feedback</p>;
   }
 
-  return (
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <div className="feedback-list">
       <AnimatePresence>
         {feedback.map((item) => (
